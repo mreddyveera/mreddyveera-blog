cc945011f99a5518d675f20b59644bee3021b4c0
mreddyveera <reddyveeramanikanta54304@gmail.com>
2025-08-20 21:31:46 +0530

    Nothing

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";   // ✅ named import
 
-export default function Signin() {
+function Signin() {
   return (
-    <div>
-      
+    <div className="min-h-screen mt-20">
+      <div className="">
+        {/*left*/}
+        <Link
+          to="/"
+          className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+        >
+          <span className="px-3 py-2 rounded-full bg-indigo-100 text-indigo-700 font-semibold">
+            Manikanta
+          </span>{" "}
+          Blog
+        </Link>
+      </div>
+      <div className="">
+        {/*right*/}
+      </div>
     </div>
-  )
+  );
 }
+
+export default Signin;
